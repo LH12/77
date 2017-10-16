3c5823648fc5334ef216ea730aa2a14e00077751
zmxlhlh <1936455932@qq.com>
2017-10-16 17:27:18 +0800

    A
    easy

---
+++ demo3.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class demo3 : MonoBehaviour {
+
+	public GameObject origin;
+	// Use this for initialization
+	void Start () {
+		//1
+//		GameObject obj = new GameObject ();
+//		obj.name="zzz";
+//		obj.transform.position = Vector3.zero;
+
+		//2
+//		GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+//		cube.name="MyCube";
+//		cube.transform.position = new Vector3 (1, 1, 1);
+//		cube.SetActive (false);
+
+	
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 o= origin.GetComponent<Transform> ().position;
+		this.transform.RotateAround (o, Vector3.up, 1);
+
+
+	}
+}
